cc351c8c3de12852e2ea9aabe3a45c959effed0e
IDGB <49776938+ivergil@users.noreply.github.com>
2019-10-21 20:58:40 -0600

    Cards Views

--- client/src/components/GroceryCard/index.js
+++ client/src/components/GroceryCard/index.js
@@ -22,35 +22,9 @@ function GroceryCard(props) {
        x
      </button> */}
       </div>
-
-
-function GroceryCard(props) {
-
-
-  return (
-    <div className="col-6 mb-4 mr-2 ml-2">
-    <div className="card" style={{width:200, height:300}} >
-      <div className="img-container" style={{height:220, backgroundSize: "cover", backgroundImage: `url(${props.image})`}}>
-       
-      </div>
-      <div className="content">
-        <h6>{props.recipeTitle}</h6>
-            
-        
-
-      </div>
-      <button  className="remove">
-        x
-      </button>
-      
-      {/* <button onClick={() => props.removeFriend(props.id)} className="remove">
-        x
-      </button> */}
-
     </div>
 
-    </div>
-  );
+  )
 }
 
 
