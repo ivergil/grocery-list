ed73c453780e7078a2f67ed3f3bf5fcddce523b5
IDGB <49776938+ivergil@users.noreply.github.com>
2019-10-21 18:25:15 -0600

    cards

--- client/src/components/RecipeCard/index.js
+++ client/src/components/RecipeCard/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import $ from 'jquery';
-// import "./style.css";
+import "./style.css";
 
 function RecipeCard(props) {
 
@@ -15,41 +15,44 @@ function RecipeCard(props) {
   // })
 
   return (
-    <div className="mb-4 mr-2 ml-2">
-    <div className="card" style={{width:300, height:400}} >
-      <div className="img-container" style={{height:220, backgroundSize: "cover", backgroundImage: `url(https://spoonacular.com/recipeImages/${props.image})`}}>
-        {/* <img alt={props.recipeTitle} src={`https://spoonacular.com/recipeImages/${props.image}`} />
+    <div >
+      <div className="card" style={{ width: 300, height: 400 }} >
+
+        <div className="img-container" style={{ height: 220, backgroundSize: "cover", backgroundImage: `url(https://spoonacular.com/recipeImages/${props.image})` }}>
+          {/* <img alt={props.recipeTitle} src={`https://spoonacular.com/recipeImages/${props.image}`} />
         <img style={{: 'cover'}} src={ `https://spoonacular.com/recipeImages/${props.image}`} /> */}
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Recipe Name:</strong> {props.recipeTitle}
-          </li>
-          <li>
-            <strong>Servings:</strong> {props.servings}
-          </li>
-          <li>
-            <strong>Ready in:</strong> {props.readyInMinutes} min
-          </li>
-          
-          <a href="#">Add Favorite</a>
+        </div>
 
-        </ul>
 
-      </div>
-      <button className="groceries">
-        My groceries
+        <div className="content">
+          <ul>
+            <li>
+              <strong>Recipe Name:</strong> {props.recipeTitle}
+            </li>
+            <li>
+              <strong>Servings:</strong> {props.servings}
+            </li>
+            <li>
+              <strong>Ready in:</strong> {props.readyInMinutes} mins
+          </li>
+
+            <a href="#">Add Favorite</a>
+
+          </ul>
+
+        </div>
+
+        
+        <button className="groceries">
+          +
       </button>
-      <button  className="remove">
-        x
-      </button>
-      
-      {/* <button onClick={() => props.removeFriend(props.id)} className="remove">
+        
+
+        {/* <button onClick={() => props.removeFriend(props.id)} className="remove">
         x
       </button> */}
-
-    </div>
+{/*  */}
+      </div>
     </div>
   );
 }
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import API from "../utils/API";
-import { Container, Box, BoxOne } from "../components/Grid";
-import {Input,SearchBtn} from "../components/SearchBox";
+import { Container, Box, BoxOne, Col, Row } from "../components/Grid";
+import { Input, SearchBtn } from "../components/SearchBox";
 import LoginNavbar from "../components/LoginNavbar";
 import Jumbotron from "../components/Jumbotron";
 import GroceryCard from "../components/GroceryCard"
@@ -23,15 +23,15 @@ class Home extends Component {
   }
 
 
-getRecipesIds = () => {
-  API.spoonacularId(this.state.title)
-    .then(res => {
-       console.log(this.state.title);
-      this.setState({ listOfResults:res.data.results });
-       console.log(this.state.listOfResults);
-    })
-    .catch(err => console.log(err));
-}
+  getRecipesIds = () => {
+    API.spoonacularId(this.state.title)
+      .then(res => {
+        console.log(this.state.title);
+        this.setState({ listOfResults: res.data.results });
+        console.log(this.state.listOfResults);
+      })
+      .catch(err => console.log(err));
+  }
 
 
   handleInputChange = event => {
@@ -45,74 +45,84 @@ getRecipesIds = () => {
   handleSubmit = event => {
     event.preventDefault();
     this.getRecipesIds(this.state.title);
-   
+
   };
 
-//   saveABook = (bookQuery) => {
-//       API.saveBook(bookQuery)
-//         .then(res => {console.log(res); this.loadBooks()})
-//         .catch(err => console.log(err));
-//   }
+  //   saveABook = (bookQuery) => {
+  //       API.saveBook(bookQuery)
+  //         .then(res => {console.log(res); this.loadBooks()})
+  //         .catch(err => console.log(err));
+  //   }
 
   render() {
     return (
       <div>
 
-          <LoginNavbar></LoginNavbar>
+        <LoginNavbar></LoginNavbar>
 
-          <Container fluid>   
+        <Container fluid>
           <Jumbotron>
-              <h1>Chef Helper</h1>
-              <p>..........................</p>
+            <h1>Chef Helper</h1>
+            <p>..........................</p>
           </Jumbotron>
-               
+
           <Box>
             <h4 className="mb-4" >Search Recipes</h4>
             <p>Key Word:</p>
-          <Input
-            value={this.state.title}
-            onChange={this.handleInputChange}
-            name="title"
-            placeholder="Chicken Teriyaki">
-          </Input>
-          <SearchBtn
-            onClick={this.handleSubmit}>
-            Search
+            <Input
+              value={this.state.title}
+              onChange={this.handleInputChange}
+              name="title"
+              placeholder="Chicken Teriyaki">
+            </Input>
+            <SearchBtn
+              onClick={this.handleSubmit}>
+              Search
           </SearchBtn>
-         </Box>
-        <BoxOne>
-        <h4 className="mb-4"> Results</h4>
-        {this.state.listOfResults.map(recipe => (
+          </Box>
+          <BoxOne>
+            <h4 className="mb-4"> Results</h4>
+            <Row>
+              {this.state.listOfResults.map(recipe => (
+                
+                <Col size="md-4">
+                  <RecipeCard
+                    id={recipe.id}
+                    key={recipe.id}
+                    //saveABook = {this.saveABook}
+                    recipeTitle={recipe.title}
+                    //authors={book.volumeInfo.authors ? book.volumeInfo.authors.join(", "): "No Available Author"}
+                    image={recipe.image}
+                    servings={recipe.servings}
+                    readyInMinutes={recipe.readyInMinutes} />
+                </Col>
 
-            <RecipeCard
-              id={recipe.id}
-              key={recipe.id}
-              //saveABook = {this.saveABook}
-              recipeTitle={recipe.title}
-              //authors={book.volumeInfo.authors ? book.volumeInfo.authors.join(", "): "No Available Author"}
-              image={recipe.image}
-              servings={recipe.servings}
-              readyInMinutes={recipe.readyInMinutes}/>
+              ))}
+            </Row>
+          </BoxOne>
+
+          <BoxOne>
+            <h4 className="mb-4"> Grocery Calculator</h4>
+            <div className="row">
+              <Col size="md-6">
+                <GroceryCard />
+              </Col>
+              <Col size="md-6">
+                Hola
+            </Col>
+            </div>
+
+          </BoxOne>
+
+          <BoxOne>
+            <h4 className="mb-4"> Coupons</h4>
+
+          </BoxOne>
 
 
-))}
 
-        </BoxOne>
-
-        <BoxOne>
-        <h4 className="mb-4"> Grocery Calculator</h4>
-        <GroceryCard/>
-        </BoxOne>
-
-        <BoxOne>
-        <h4 className="mb-4"> Coupons</h4>
-        
-        </BoxOne>
-
-        
-
-         </Container>
-      {/* <Nav
+        </Container>
+        {/* <Nav
       Home = {this.state.Home} 
       saved = {this.state.saved}/>
       <Container fluid>
@@ -160,7 +170,7 @@ getRecipesIds = () => {
 
      </Container> */}
       </div>
-     
+
     );
   }
 }
