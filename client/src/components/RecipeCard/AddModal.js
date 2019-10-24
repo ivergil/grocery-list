9100a06fe1e6ad52eee5a4bafee8f04eb0d41102
Nathalie Solis-Maravilla <nathaliesolis-maravilla@nathalies-mbp.uconnect.utah.edu>
2019-10-23 19:05:53 -0600

    merge master

--- client/src/components/RecipeCard/AddModal.js
+++ client/src/components/RecipeCard/AddModal.js
@@ -1,33 +1,43 @@
-import React, { Component } from 'react';
-import { Modal, Button, Row, Col, Form } from 'react-bootstrap';
+// import React, { Component } from 'react';
+// import { Modal, Button, Row, Col, Form } from 'react-bootstrap';
 
-export class addModal extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <Modal
-                {...this.props}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        Recipe Description
-        </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
+// export class addModal extends Component {
+//     constructor(props) {
+//         super(props);
+//     }
+//     render() {
+//         return (
+//             <Modal
+//                 {...this.props}
+//                 size="lg"
+//                 aria-labelledby="contained-modal-title-vcenter"
+//                 centered
+//             >
+//                 <Modal.Header closeButton>
+//                     <Modal.Title id="contained-modal-title-vcenter">
+//                         Recipe Description
+//         </Modal.Title>
+//                 </Modal.Header>
+//                 <Modal.Body>
 
-                < div className = "container">
+//                 < div className = "container">
                     
-                </div>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button S>Close</Button>
-                </Modal.Footer>
-            </Modal>
-        );
+//                 </div>
+//                 </Modal.Body>
+//                 <Modal.Footer>
+//                     <Button S>Close</Button>
+//                 </Modal.Footer>
+//             </Modal>
+//         );
+//     }
+// }
+
+import React from "react";
+import Modal from "./Component/Modal";
+import "./"
+
+export default class Modal extends React.Component{
+    render(){
+        return <div>Hello Modal</div>;
     }
 }
